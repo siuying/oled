c41c257a7d2eed810d5ea81b4e231ef2ea2e69a9
Francis Chong <francis@goodnotes.com>
2018-09-20 19:51:03 +0800

    update script

--- stats.sh
+++ stats.sh
@@ -3,4 +3,4 @@ cpu=`top -bn1 | grep load | awk '{printf "CPU Load: %.2f", $(NF-2)}'`
 memory=`free -m | awk 'NR==2{printf "Mem: %s/%sMB %.2f%%", $3,$2,$3*100/$2 }'`
 disk=`df -h | awk '$NF=="/"{printf "Disk: %.1f/%.1fGB %s", $3,$2,$5}'`
 
-./oled "./start.ttf" "IP: ${host}" "$cpu" "$memory" "$disk"
+oled "./start.ttf" "IP: ${host}" "$cpu" "$memory" "$disk"
